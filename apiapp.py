21a40e9001d4e33da8ecc9bf119d87e11be7442b
Jorge Enrique Gamboa Fuentes <snake2209@hotmail.com>
2023-12-11 00:43:27 -0500

    First commit

---
+++ apiapp.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+richest_people_list = {
+    "Elon Mu": "200 billion",
+    "Jeff B": "199 billion",
+    "Bill Gates": "198 billion",
+    "Mark, Z": "197 billion"
+}
+
+@app.get("/richest-people")
+def richest_people():
+    return richest_people_list
